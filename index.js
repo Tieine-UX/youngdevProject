d653ab21c46bd897057828b0968075ea7dfba2db
Tieine-UX <mommsm254@gmail.com>
2026-03-24 11:01:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function area(length, width) {
-    var result = parseInt(length) * parseInt(width);
-    document.getElementById("output").innerHTML = result;
-}
+const http = require ('http')
+const server = http.createServer((req , res) => {
+    res.writeHead (200 , {'Content -Type' : 'text/ plain' });
+    res.end ('Hello W orld');
+});
+server.listen (3000 , () => {
+console.log (`Server is running at http://localhost:3000/`);
+});
+
+
